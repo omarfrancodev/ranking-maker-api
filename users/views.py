451b7932685ff7c5a9e3d033ecfa192e0c8286e0
Omar Franco <fofe2803@gmail.com>
2024-09-01 16:47:42 -0600

    feat (users): add mixin for the custom response hanlder

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from .models import Person
 from .serializers import PersonSerializer
+from utilities.mixins import CustomResponseMixin
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(CustomResponseMixin, viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer

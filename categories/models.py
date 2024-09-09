17dc53628b084a04e1a5f8a50dddec5d7aef7a6d
Omar Franco <fofe2803@gmail.com>
2024-09-09 01:29:11 -0600

    style: formatting the document

--- categories/models.py
+++ categories/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 import uuid
 
+
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ['name']
+
 
 class Subcategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='subcategories')
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ['name']
-        unique_together = ['name', 'category']
+        unique_together = ('name', 'category')

1921e2ab4e54db6fb210ed4eaf07f57b19feccd2
Omar Franco <fofe2803@gmail.com>
2024-09-09 01:34:44 -0600

    feat (content): changes one the structure of how to manage the content and the relationship with other apps (models)

--- content/serializers.py
+++ content/serializers.py
@@ -1,50 +1,116 @@
 from rest_framework import serializers
 from .models import Content
+from categories.serializers import CategorySerializer, SubcategoryReadOnlySerializer
 from categories.models import Category, Subcategory
+from viewings.models import Viewing
+from users.serializers import PersonSerializer
+from users.models import Person
 
-class ContentSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    subcategories = serializers.PrimaryKeyRelatedField(queryset=Subcategory.objects.all(), many=True)
+
+class ContentCreateUpdateSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    subcategories = serializers.PrimaryKeyRelatedField(
+        queryset=Subcategory.objects.all(),
+        many=True
+    )
 
     class Meta:
         model = Content
         fields = ['id', 'name', 'category', 'subcategories']
 
-    def validate(self, data):
-        category = data.get('category')
-        subcategories = data.get('subcategories', [])
+    def validate(self, attrs):
+        instance = getattr(self, 'instance', None)
+        category = attrs.get('category', getattr(instance, 'category', None))
+        subcategories = attrs.get('subcategories', getattr(
+            instance, 'subcategories', None))
 
-        # Ensure subcategories belong to the given category
-        for subcategory in subcategories:
-            if subcategory.category != category:
+        if category and subcategories:
+            invalid_subcategories = [
+                sub for sub in subcategories if sub.category != category]
+            if invalid_subcategories:
                 raise serializers.ValidationError({
-                    'subcategories': f'Subcategory {subcategory.name} does not belong to the selected category.'
+                    'subcategories': f"Las siguientes subcategorías no pertenecen a la categoría seleccionada: {', '.join(str(sub) for sub in invalid_subcategories)}"
                 })
 
-        return data
+        return attrs
 
     def create(self, validated_data):
-        category = validated_data.pop('category')
-        subcategories = validated_data.pop('subcategories')
-
-        content = Content.objects.create(category=category, **validated_data)
-
-        # Set valid subcategories that belong to the selected category
+        subcategories = validated_data.pop('subcategories', [])
+        content = Content.objects.create(**validated_data)
         content.subcategories.set(subcategories)
-
         return content
 
     def update(self, instance, validated_data):
-        category = validated_data.pop('category', None)
-        subcategories = validated_data.pop('subcategories', None)
-
-        if category:
-            instance.category = category
-
-        if subcategories is not None:
-            # Set valid subcategories that belong to the selected category
-            instance.subcategories.set(subcategories)
-
-        instance.name = validated_data.get('name', instance.name)
+        if 'category' in validated_data:
+            instance.category = validated_data['category']
+        if 'name' in validated_data:
+            instance.name = validated_data['name']
+        if 'subcategories' in validated_data:
+            instance.subcategories.set(validated_data['subcategories'])
         instance.save()
         return instance
+
+
+class ContentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Content
+        fields = ['id', 'name']
+
+
+class ContentDetailSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    subcategories = SubcategoryReadOnlySerializer(many=True)
+
+    class Meta:
+        model = Content
+        fields = ['id', 'name', 'category', 'subcategories']
+
+
+class ViewingDetailSerializer(serializers.ModelSerializer):
+    person = PersonSerializer()
+
+    class Meta:
+        model = Viewing
+        fields = ['person', 'date_viewed']
+
+
+class ContentWithViewingsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    subcategories = SubcategoryReadOnlySerializer(many=True)
+    viewing = serializers.SerializerMethodField()
+    views = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Content
+        fields = ['id', 'name', 'category',
+                  'subcategories', 'viewing', 'views']
+
+    def get_viewing(self, obj):
+        viewings = Viewing.objects.filter(content=obj)
+        return ViewingDetailSerializer(viewings, many=True).data
+
+    def get_views(self, obj):
+        return Viewing.objects.filter(content=obj).count()
+
+
+class ContentFilterSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    subcategories = SubcategoryReadOnlySerializer(many=True)
+    persons = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Content
+        fields = ['id', 'name', 'category', 'subcategories', 'persons']
+
+    def get_persons(self, obj):
+        viewings = Viewing.objects.filter(content=obj)
+        return [
+            {
+                'person_id': viewing.person.id,
+                'name': viewing.person.name,
+                'date_viewed': viewing.date_viewed
+            }
+            for viewing in viewings
+        ]
--- content/views.py
+++ content/views.py
@@ -1,21 +1,73 @@
-from rest_framework import viewsets
-from .models import Content
-from .serializers import ContentSerializer
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Count, Prefetch
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Content
 from utilities.mixins import CustomResponseMixin
+from .serializers import (
+    ContentCreateUpdateSerializer,
+    ContentListSerializer,
+    ContentDetailSerializer,
+    ContentWithViewingsSerializer,
+    ContentFilterSerializer
+)
+
 
 class ContentViewSet(CustomResponseMixin, viewsets.ModelViewSet):
     queryset = Content.objects.all()
-    serializer_class = ContentSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category__id', 'subcategories__id']
+    search_fields = ['name']
+    ordering_fields = ['name']
+    ordering = ['name']
 
-    @action(detail=False, methods=['get'])
-    def with_view_counts(self, request):
-        queryset = Content.objects.annotate(view_count=Count('viewings'))
-        page = self.paginate_queryset(queryset)
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ContentListSerializer
+        elif self.action == 'retrieve':
+            return ContentDetailSerializer
+        elif self.action == 'list_with_viewings':
+            return ContentWithViewingsSerializer
+        elif self.action == 'filter_contents':
+            return ContentFilterSerializer
+        else:
+            return ContentCreateUpdateSerializer
+
+    @action(detail=False, methods=['get'], url_path='with-viewings')
+    def list_with_viewings(self, request):
+        contents = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(contents)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(queryset, many=True)
+
+        serializer = self.get_serializer(contents, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='filter')
+    def filter_contents(self, request):
+        category_id = request.query_params.get('category')
+        subcategory_id = request.query_params.get('subcategory')
+        person_id = request.query_params.get('person')
+
+        queryset = self.get_queryset()
+
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
+        if subcategory_id:
+            queryset = queryset.filter(subcategories__id=subcategory_id)
+        if person_id:
+            queryset = queryset.filter(viewings__person__id=person_id)
+
+        queryset = queryset.distinct()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(
+                page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(
+            queryset, many=True, context={'request': request})
         return Response(serializer.data)
